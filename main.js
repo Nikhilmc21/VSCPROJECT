7a52cb2665d045bd61947b9c04d4a6884f0a8ec6
Nikhilmc21 <84916563+Nikhilmc21@users.noreply.github.com>
2021-09-16 18:16:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firststName = "Nikhil";
+var secondName = "Singh";
+console.log(firststName);
+console.log(secondName);
+var c = firststName.concat(secondName);
+console.log(c);
+var a = 25;
+console.log(a);
+var b = 26;
+console.log(b);
+var c = a+b;
+console.log(c)
